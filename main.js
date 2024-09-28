3e22ae46362b10f1a8e10d180ca255c5452c35c1
Daria Sibova <dariasib05@gmail.com>
2024-09-28 15:41:28 +0300

    commit

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ let search = document.querySelector('.search-box');
 
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
+    navbar.classList.remove('active');
 }
 
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-icon').onclick = () => {
     navbar.classList.toggle('active');
+    search.classList.remove('active');
 }
 
 
